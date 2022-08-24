3f2c4bbc0eda64e358e6cec4e26cf7dbf48a2bea
Ilya Klishevitch <klishevich.rabota122@gmail.com>
2022-08-24 15:17:05 +0300

    Add Paginate

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -13,7 +13,7 @@ class HomeNews(MyMixin, ListView):
     template_name = 'news/home_news_list.html'
     context_object_name = 'news'
     mixin_prop = 'Hello world'
-    # extra_context = {'title': 'Главная'}
+    paginate_by = 2
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -36,6 +36,7 @@ class NewsByCategory(MyMixin, ListView):
     template_name = 'news/home_news_list.html'
     context_object_name = 'news'
     allow_empty = False
+    paginate_by = 2
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
