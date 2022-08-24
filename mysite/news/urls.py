65e3437ac28760f658adfee00a65fac536a17e8a
Ilya Klishevitch <klishevich.rabota122@gmail.com>
2022-08-24 19:07:39 +0300

    Add Regiser and Login

--- mysite/news/urls.py
+++ mysite/news/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('register/', register, name='register'),
+    path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
     #  path('', index, name='home'),
     path('', HomeNews.as_view(), name='home'),
     # path('category/<int:category_id>/', get_category, name='category'),
